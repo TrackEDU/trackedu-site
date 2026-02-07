f461c617229d25563dee9b5818dfd0070fadee92
TrackEDU <Hello@trackedu.net>
2026-02-07 22:30:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,20 @@
-const CACHE_NAME = 'trackedu-v4';
+const CACHE_NAME = 'trackedu-v6';
 const ASSETS = [
   '/app/',
-  '/app-icon.png' // New branded icon
+  'https://trackedu.github.io/common-assets/studentapp.png',
+  'https://trackedu.github.io/common-assets/teacherapp.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    // THE FIX: "ignoreSearch: true" allows the cache to work with any ?id= school
     caches.match(event.request, { ignoreSearch: true }).then((response) => {
       return response || fetch(event.request);
     })
