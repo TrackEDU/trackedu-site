d371f3f261f1ea88bc649e7d5054247cc86f9bf4
TrackEDU <Hello@trackedu.net>
2026-02-07 22:49:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trackedu-v6';
+const CACHE_NAME = 'trackedu-v8'; // Increment this whenever you update the registry
 const ASSETS = [
   '/app/',
   'https://trackedu.github.io/common-assets/studentapp.png',
@@ -7,8 +7,21 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // Forces the new service worker to become active immediately
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Deletes old versions to save space
+          }
+        })
+      );
     })
   );
 });
