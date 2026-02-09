55ad110be708c62d48867b6f233613f3c5afd34b
TrackEDU <Hello@trackedu.net>
2026-02-09 16:14:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,48 @@
-const CACHE_NAME = 'trackedu-v15'; // Bumped to v15 to force update
+const CACHE_NAME = 'trackedu-v14'; // I bumped this to v14 for you
 const ASSETS = [
-  'https://trackedu.github.io/common-assets/studentapp.png',
-  'https://trackedu.github.io/common-assets/teacherapp.png',
-  'https://trackedu.github.io/common-assets/teloading.png' // Added new loading image
+'https://trackedu.github.io/common-assets/studentapp.png',
+'https://trackedu.github.io/common-assets/teacherapp.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
+event.waitUntil(
+caches.keys().then((cacheNames) => {
+return Promise.all(
+cacheNames.map((cacheName) => {
+if (cacheName !== CACHE_NAME) {
+return caches.delete(cacheName);
+}
+})
+);
+})
+);
+self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => response || fetch(event.request))
-    );
-  }
+// 1. FOR THE HTML FILE (The App Itself) -> NETWORK FIRST
+// This ensures users always get the latest School Registry
+if (event.request.mode === 'navigate') {
+event.respondWith(
+fetch(event.request)
+.catch(() => {
+return caches.match(event.request);
+})
+);
+}
+// 2. FOR IMAGES & ASSETS -> CACHE FIRST (Fast loading)
+else {
+event.respondWith(
+caches.match(event.request).then((response) => {
+return response || fetch(event.request);
+})
+);
+}
 });
